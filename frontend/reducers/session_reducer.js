f655c8575b294a601b842146dc77bab12c77838f
joe-delaney <joseph.delaney546@gmail.com>
2022-04-21 15:42:53 -0400

    finished session reducer

---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,20 @@
+import { RECEIVE_CURRENT_USER, REMOVE_CURRENT_USER } from "../actions/session_actions";
+
+const _nullState = {
+    currentUserId: null
+}
+
+export const SessionReducer = (state = _nullState, action) => {
+    Object.freeze(state);
+
+    switch (action.type) {
+        case RECEIVE_CURRENT_USER:
+            let nextState = Object.assign({}, state);
+            nextState["currentUserId"] = action.currentUser.id;
+            return nextState;
+        case REMOVE_CURRENT_USER:
+            return _nullState;
+        default:
+            return state;
+    }
+}
