33e8c649e7342ece876ca1b64495d0b7297a7316
joe-delaney <joseph.delaney546@gmail.com>
2022-04-21 16:34:55 -0400

    added app and root components

--- frontend/slack_chat.jsx
+++ frontend/slack_chat.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Root from "./components/root";
 import configureStore from "./store/store"
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = document.getElementById("root");
-    window.store  = configureStore();
-    ReactDOM.render(<h1>Welcome to SlackChat</h1>, root);
+    ReactDOM.render(<Root store={configureStore()}/>, root);
 });
