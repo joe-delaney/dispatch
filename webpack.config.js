3559c8fc0164d5c7c4c4825f3dda1c6813bf500e
joe-delaney <joseph.delaney546@gmail.com>
2022-04-21 14:36:47 -0400

    finished frontend config and setup

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+
+module.exports = {
+    entry: './frontend/slack_chat.jsx',
+    output: {
+        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+        filename: 'bundle.js'
+    },
+    devtool: 'source-map',
+    module: {
+        rules: [
+            {
+                test: [/\.jsx?$/],
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/env', '@babel/react']
+                    }
+                },
+            }
+        ]
+    },
+    resolve: {
+        extensions: [".js", '.jsx', '*']
+    }
+};
