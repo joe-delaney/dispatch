c987f55c86bfc5b001ba000c05b95ef37ed52b76
joe-delaney <joseph.delaney546@gmail.com>
2022-04-23 20:15:38 -0400

    working on logged out splash page

---
+++ frontend/components/splash_page/main_splash_page_section.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const MainSplashPageSection = () => {
+
+    return (
+        <div className="splash-page-section-main logged-out">
+            <div className="splash-page-section-main-left">
+                <h1 className="splash-page-section-main-header">Slack is your digital HQ</h1>
+                <p className="splash-page-section-main-description">Transform the way you work with one place for everyone and everything you need to get stuff done.</p>
+            </div>
+            <div className="splash-page-section-main-right">
+
+            </div>
+        </div>
+    )
+}
+
+export default MainSplashPageSection;
--- frontend/components/splash_page/splash_page.jsx
+++ frontend/components/splash_page/splash_page.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import WelcomeNavBar from "./welcome_nav_bar";
+import MainSplashPageSection from "./main_splash_page_section";
 
 const SplashPage = ({currentUser, logout, demoClicked}) => (
     <div className="splash-page">
-        <section className="top-splash-page-section">
+        <section>
             <WelcomeNavBar logout={logout} currentUser={currentUser} demoClicked={demoClicked}/>
+        </section>
+        <section>
+            <MainSplashPageSection currentUser={currentUser}/>
         </section>
     </div>
 );
