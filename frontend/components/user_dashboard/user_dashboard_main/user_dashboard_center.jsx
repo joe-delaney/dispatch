7fef6b8327dd1ccf7d04e8f2e25a32e27af8ab24
joe-delaney <joseph.delaney546@gmail.com>
2022-04-25 11:06:38 -0400

    added in message input field

--- frontend/components/user_dashboard/user_dashboard_main/user_dashboard_center.jsx
+++ frontend/components/user_dashboard/user_dashboard_main/user_dashboard_center.jsx
@@ -11,7 +11,10 @@ export default class UserDashboardCenter extends React.Component {
 
                 </div>
                 <div className="user-dashboard-center-main-message-composer">
-
+                    <div className="user-dashboard-message-container">
+                        <textarea type="text" className="message-input" placeholder="Write your message here..."/>
+                            <button className="send-message-button">Send</button>
+                    </div>
                 </div>
             </div>
 
