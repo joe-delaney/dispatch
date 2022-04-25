6be45b474e2a551f7b174e670226f0c4e2a611d4
joe-delaney <joseph.delaney546@gmail.com>
2022-04-25 16:03:11 -0400

    added and styled user search results

--- frontend/components/user_dashboard/user_dashboard.jsx
+++ frontend/components/user_dashboard/user_dashboard.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import UserDashboardNav from "./user_dashboard_nav";
+import UserDashboardNav from "./user_dashboard_main/user_dashboard_nav/user_dashboard_nav";
 import UserDashboardMain from "./user_dashboard_main/user_dashboard_main";
 
 export default class UserDashboard extends React.Component {
--- frontend/components/user_dashboard/user_dashboard_main/user_dashboard_nav/user_dashboard_nav.jsx
+++ frontend/components/user_dashboard/user_dashboard_main/user_dashboard_nav/user_dashboard_nav.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import UserDashBoardNavSearchResult from "./user_dashboard_nav_search_result";
 
 class UserDashboardNav extends React.Component {
 
@@ -23,11 +24,18 @@ class UserDashboardNav extends React.Component {
     }
 
     render() {
+        const searchResults = (this.props.userSearchResults.length || !this.state.query.length) ? this.props.userSearchResults : [{displayName: "No results found"}];
+
         return (
             <nav className="user-dashboard-nav-bar">
                 <div className="user-dashboard-nav-bar-left"></div>
                 <div className="user-dashboard-nav-bar-center">
                     <input onChange={this.handleInput} className="user-dashboard-nav-bar-search-input" type="text" value={this.state.query} placeholder="&#xF002;  Search Slack"/>
+                    <ul className="user-dashboard-nav-bar-search-results">
+                        {searchResults.map((user) => (
+                            <UserDashBoardNavSearchResult user={user}/>
+                        ))}
+                    </ul>
                 </div>
                 <div className="user-dashboard-nav-bar-right"></div>
             </nav>
