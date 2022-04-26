6d0b55b5d56e9e06db34faf84356fc5fc0913e86
joe-delaney <joseph.delaney546@gmail.com>
2022-04-25 23:27:51 -0400

    finished current user show page

--- frontend/components/user_dashboard/user_show/user_show.jsx
+++ frontend/components/user_dashboard/user_show/user_show.jsx
@@ -11,6 +11,10 @@ export default class UserShow extends React.Component {
     }
 
     render() {
+
+        const displayName = this.props.user ? this.props.user.displayName : "";
+        const title = (this.props.user && this.props.user.title) ? this.props.user.title : "";
+
         return (
             <div className="user-dashboard-user-show">
                 <div className="user-show-header">
@@ -19,7 +23,20 @@ export default class UserShow extends React.Component {
                         <span>&times;</span>
                     </Link>
                 </div>
-                <span>{this.props.user.displayName}</span>
+                <div className="user-show-details">
+                    <div className="user-show-image">
+                        <strong className="user-show-initial">{displayName[0]}</strong>
+                    </div>
+                    <strong className="user-show-display-name">{displayName}</strong>
+                    <span className="user-show-title">{title}</span>
+                    <div className="user-show-action">
+                        <button className="user-show-button">
+                            <img src={window.pencilIcon} alt="pencil" />
+                        </button>
+                        <span className="user-show-action-label">Edit profile</span>
+                    </div>
+                    
+                </div>
             </div>
         )
     }
