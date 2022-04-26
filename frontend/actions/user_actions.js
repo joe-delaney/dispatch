b599f4da66abcbcc2fc4854403382c3d10c38b87
joe-delaney <joseph.delaney546@gmail.com>
2022-04-26 09:49:32 -0400

    added update thunk action and updated user reducer

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,12 +1,17 @@
 import * as UserAPIUtil from "../util/user_api_util";
 
-export const RECEIVE_SELECTED_USER = "RECEIVE_SELECTED_USER"
+export const RECEIVE_USER = "RECEIVE_USER";
+export const UPDATE_USER = "UPDATE_USER";
 
-export const receiveSelectedUser = (user) => ({
-    type: RECEIVE_SELECTED_USER,
+export const receiveUser = (user) => ({
+    type: RECEIVE_USER,
     user
 });
 
 //fetch selected user thunk action
 export const fetchUser = (userId) => dispatch => UserAPIUtil.fetchUser(userId)
-    .then(user => dispatch(receiveSelectedUser(user)))
+    .then(user => dispatch(receiveUser(user)))
+
+//update user thunk action
+export const updateUser = (user) => dispatch => UserAPIUtil.updateUser(user)
+    .then(user => dispatch(receiveUser(user)))
--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,14 +1,16 @@
 import { RECEIVE_CURRENT_USER } from "../../actions/session_actions";
-import { RECEIVE_SELECTED_USER } from "../../actions/user_actions";
+import { RECEIVE_USER } from "../../actions/user_actions";
 
 export const usersReducer = (state = {}, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, { [action.currentUser.id]: action.currentUser });
-        case RECEIVE_SELECTED_USER: 
+        case RECEIVE_USER: 
             //This should add the selected user to state so we can access their info
-            return Object.assign({}, state, { [action.user.id]: action.user});
+            let nextState = Object.assign({}, state);
+            nextState[action.user.id] = action.user;
+            return nextState;
         default:
             return state;
     }
