610764e444a1b259085296a303804b9ce8f478f7
joe-delaney <joseph.delaney546@gmail.com>
2022-04-26 14:25:26 -0400

    added error handling to edit modal

--- frontend/components/modals/user_edit_modal.jsx
+++ frontend/components/modals/user_edit_modal.jsx
@@ -15,7 +15,8 @@ export default class UserEditModal extends React.Component {
             id: this.props.currentUser.id,
             display_name: this.props.currentUser.displayName,
             title: title,
-            status: status
+            status: status,
+            displayNameError: false
         }
     }
 
@@ -34,7 +35,8 @@ export default class UserEditModal extends React.Component {
             id: this.props.currentUser.id,
             display_name: this.props.currentUser.displayName,
             title: title,
-            status: status
+            status: status,
+            displayNameError: false
         })
         this.props.toggleEditModal();
     }
@@ -52,10 +54,19 @@ export default class UserEditModal extends React.Component {
 
     render() {
         const editModalClass = this.props.editModalDisplayed ? "user-edit-modal" : "user-edit-modal hidden"
+        const errorMessage = !this.state.display_name.length ? (
+            <div className="edit-error">
+                 <p className="edit-error-icon">⚠</p>
+                 <label for="displayNameInput" className="edit-error-message">Unfortunately, you can’t leave this blank.</label>
+            </div>) : "";
+
+        const inputErrorActive = !this.state.display_name.length ? "edit-error-active" : "";
+        const descriptionErrorActive = !this.state.display_name.length ? "description-error-active" : "";
+        const containerErrorActive = !this.state.display_name.length ? "user-edit-container-error-active" : "";
 
         return (
             <section className={editModalClass} >
-                <div className="user-edit-modal-content-container">
+                <div className={`user-edit-modal-content-container ${containerErrorActive}`}>
                     <div className="user-edit-modal-header">
                         <div className="user-edit-modal-title">Edit your profile</div>
                         <button onClick={this.closeModal} className="close-edit-user-modal-button">
@@ -66,17 +77,18 @@ export default class UserEditModal extends React.Component {
                         <div className="user-edit-modal-inputs">
                             <div className="user-edit-modal-input-container">
                                 <label for="displayNameInput" className="user-edit-modal-input-label">Display Name</label>
-                                <input id="displayNameInput" className= "user-edit-modal-input" type="text" value={this.state.display_name} onChange={this.handleInput("display_name")} />
-                                <span className="user-edit-modal-input-description">This could be your first name, or a nickname — however you’d like people to refer to you in Slack.</span>
+                                <input autocomplete="off" id="displayNameInput" className= {`user-edit-modal-input ${inputErrorActive}`} type="text" value={this.state.display_name} onChange={this.handleInput("display_name")} />
+                                <span className={`user-edit-modal-input-description ${descriptionErrorActive}`}>This could be your first name, or a nickname — however you’d like people to refer to you in Slack.</span>
+                                {errorMessage}
                             </div>
                             <div className="user-edit-modal-input-container">
                                 <label for="titleInput" className="user-edit-modal-input-label">What I do</label>
-                                <input id="titleInput" className="user-edit-modal-input" type="text" value={this.state.title} onChange={this.handleInput("title")} placeholder="title (optional)" />
+                                <input autocomplete="off" id="titleInput" className="user-edit-modal-input" type="text" value={this.state.title} onChange={this.handleInput("title")} placeholder="title (optional)" />
                                 <span className="user-edit-modal-input-description">Let people know what you do.</span>
                             </div>
                             <div className="user-edit-modal-input-container">
                                 <label for="statusInput" className="user-edit-modal-input-label">Status</label>
-                                <input id="statusInput" className="user-edit-modal-input" type="text" value={this.state.status} onChange={this.handleInput("status")} placeholder="status (optional)" />
+                                <input autocomplete="off" id="statusInput" className="user-edit-modal-input" type="text" value={this.state.status} onChange={this.handleInput("status")} placeholder="status (optional)" />
                                 <span className="user-edit-modal-input-description">Let people know what you are currently up to.</span>
                             </div>
                         </div>
