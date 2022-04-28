237a0091187113a831ced38c3f94067fa94d3cf5
joe-delaney <joseph.delaney546@gmail.com>
2022-04-27 21:34:50 -0400

    added channels sidebar animation

--- frontend/components/channels/channel_sidebar_list/channel_sidebar_list.jsx
+++ frontend/components/channels/channel_sidebar_list/channel_sidebar_list.jsx
@@ -4,15 +4,29 @@ import ChannelSidebarListItem from "./channel_sidebar_list_item";
 class ChannelSidebarList extends React.Component {
     constructor(props) {
         super(props);
+        this.state = {
+            showChannels: true
+        }
+
+        this.toggleShowChannels = this.toggleShowChannels.bind(this);
+    }
+
+    toggleShowChannels(e) {
+        this.setState({
+            showChannels: !this.state.showChannels
+        })
     }
 
     render() {
+        const rotateArrow = this.state.showChannels ? "" : "rotate-90";
+        const hidden = this.state.showChannels ? "" : "hidden";
+
         return (
             <div className="sidebar-list">
                 <div className="sidebar-list-header">
-                    <div className="sidebar-list-header-left">
+                    <div onClick={this.toggleShowChannels} className="sidebar-list-header-left">
                         <div className="sidebar-button-image-container">
-                            <img src={window.downArrow} alt="down arrow" className="sidebar-button"></img>
+                            <img src={window.downArrow} alt="down arrow" className={`sidebar-button ${rotateArrow}`}></img>
                         </div>
                         <span className="sidebar-list-header-title">Channels</span>
                     </div>
@@ -20,11 +34,11 @@ class ChannelSidebarList extends React.Component {
                         <img src={window.plus} alt="plus" className="sidebar-button-plus"></img>
                     </div>
                 </div>
-                <ul>
+                <ul className={`${hidden}`}>
                     {this.props.channels.map((channel, idx) => <ChannelSidebarListItem key={idx} channel={channel} />)}
                 </ul>
                 <ul>
-                    <li className="sidebar-list-item last-item">
+                    <li className={`sidebar-list-item last-item ${hidden}`}>
                         <div className="add-margin-right">
                             <div className="sidebar-button-image-container-bottom">
                                 <img src={window.plusThick} alt="plus" className="sidebar-button-plus-bottom"></img>
