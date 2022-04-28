1698afe55681701e63cae7c71b8f4b9b917d7199
joe-delaney <joseph.delaney546@gmail.com>
2022-04-28 15:21:25 -0400

    fixed channel show header

--- frontend/components/channels/channel_show/channel_show_header.jsx
+++ frontend/components/channels/channel_show/channel_show_header.jsx
@@ -2,14 +2,18 @@ import React from "react";
 
 const ChannelShowHeader = ({channel, toggleModal}) => {
     const channelName = channel ? channel.name : "";
+    const channelTopic = (channel && channel.topic) ? channel.topic : "";
     const channelMembers = channel ? channel.channelMemberIds.length : 0;
     const channelMembersLabel = channelMembers === 1 ? " member" : " members";
 
     return (
         <div className="channel-show-header">
-            <div onClick={toggleModal} className="channel-show-header-text">
-                <h1 >{`# ${channelName}`}</h1>
-                <span className="channel-show-down-carret">ˇ</span>
+            <div className="channel-show-header-left">
+                <div onClick={toggleModal} className="channel-show-header-text">
+                    <h1 >{`# ${channelName}`}</h1>
+                    <span className="channel-show-down-carret">ˇ</span>
+                </div>
+                <span className="channel-show-topic">{channelTopic}</span>
             </div>
             <div onClick={toggleModal} className="channel-show-members-icon">
                 <span className="channel-show-members-icon-label">{`${channelMembers}${channelMembersLabel}`}</span>
