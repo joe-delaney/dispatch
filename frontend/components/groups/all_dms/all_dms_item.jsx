4c113072740eed603e2036e1a7a020d59c6432ba
joe-delaney <joseph.delaney546@gmail.com>
2022-05-03 15:31:46 -0400

    finished styling all dms view

--- frontend/components/groups/all_dms/all_dms_item.jsx
+++ frontend/components/groups/all_dms/all_dms_item.jsx
@@ -7,8 +7,22 @@ export default class AllDMsItem extends React.Component {
 
     render() {
         const groupName = this.props.group ? this.props.group.name : "";
+        const groupNameInitial = this.props.group ? this.props.group.name[0] : "";
         return (
-            <li>{groupName}</li>
+            <li className="all-dms-item">
+                <div className="all-dms-item-image">
+                    <strong className="all-dms-item-initial">{groupNameInitial}</strong>
+                </div>
+                <div className="all-dms-item-right">
+                    <div className="all-dms-item-content">
+                        <strong className="all-dms-group-name">{groupName}</strong>
+                        <span className="all-dms-last-message">You: Insert message here</span>
+                    </div>
+                    <div className="all-dms-item-time-label">
+                        <span>10:00 AM</span>
+                    </div>
+                </div>
+            </li>
         )
     }
 }
