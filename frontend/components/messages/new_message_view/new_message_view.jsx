33d53326b18c6216d456018bca2a31f9bfc0bc17
joe-delaney <joseph.delaney546@gmail.com>
2022-05-04 12:56:20 -0400

    created and styled selected user item

--- frontend/components/messages/new_message_view/new_message_view.jsx
+++ frontend/components/messages/new_message_view/new_message_view.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import NewMessageViewUserSearchResult from "./new_message_view_user_search_result";
 import NewMessageViewChannelSearchResult from "./new_message_view_channel_search_result";
+import SelectedUserItem from "./selected_user_item";
 
 export default class NewMessageView extends React.Component {
     constructor(props) {
@@ -43,27 +44,29 @@ export default class NewMessageView extends React.Component {
     }
 
     render() {
-        const channels = this.state.query === "" ? [] : this.props.selectedChannels(this.state.query);
+        const channels = (this.state.query === "" || this.state.selectedUsers.length) ? [] : this.props.selectedChannels(this.state.query);
         const searchResults = (this.props.userSearchResults.length || !this.state.query.length || channels.length) ? this.props.userSearchResults : [{ displayName: "No results found" }];
+        const placeholderText = this.state.selectedUsers.length ? "" : "#channel or @somebody"
+
         return (
             <div className="user-dashboard-center-main">
                 <div className="user-dashboard-center-main-feed">
                     <div className="all-dms-container">
-                        <ul className="all-dms-items">
-                            
-                        </ul>
+                        <ul className="all-dms-items"></ul>
                     </div>
                     <div className="search-bar">
                         <div className="search-bar-top">
                             <span className="search-bar-label">To: </span>
-                            {this.state.selectedUsers.map((user) => (
-                                console.log(user)
-                            ))}
+                            <ul className="selected-users">
+                                {this.state.selectedUsers.map((user, idx) => (
+                                    <SelectedUserItem key={idx} user={user} />
+                                ))}
+                            </ul> 
                             <input className="search-bar-input"
                                 type="text"
                                 onChange={this.handleInput}
                                 value={this.state.query}
-                                placeholder="#channel or @somebody" />
+                                placeholder={placeholderText} />
                         </div>
                         <ul className="search-results">
                             {searchResults.map((user) => (
