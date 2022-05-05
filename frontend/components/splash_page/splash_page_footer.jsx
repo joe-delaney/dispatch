59d3a925c22e1179b5cb7d15643f40640440212d
joe-delaney <joseph.delaney546@gmail.com>
2022-05-05 14:47:05 -0400

    added technologies to footer

--- frontend/components/splash_page/splash_page_footer.jsx
+++ frontend/components/splash_page/splash_page_footer.jsx
@@ -9,12 +9,21 @@ const SplashPageFooter = () => (
                 className="splash-page-footer-logo"
             />
         </Link>
-        <ul className="splash-page-footer-links">
-            <h3 className="splash-page-footer-h3">Personal Links</h3>
-            <a className="splash-page-footer-link" href="#">Portfolio</a>
-            <a className="splash-page-footer-link" href="https://www.linkedin.com/in/joseph-delaney-46985b76/">LinkedIn</a>
-            <a className="splash-page-footer-link" href="https://github.com/joe-delaney/slack-chat">Github</a>
-        </ul>
+        <div className="splash-page-footer-links-container">
+            <ul className="splash-page-footer-links">
+                <h3 className="splash-page-footer-h3">Technologies</h3>
+                <span className="splash-page-footer-span">React</span>
+                <span className="splash-page-footer-span">Redux</span>
+                <span className="splash-page-footer-span">Javascript</span>
+                <span className="splash-page-footer-span">Ruby on Rails</span>
+            </ul>
+            <ul className="splash-page-footer-links">
+                <h3 className="splash-page-footer-h3">Personal Links</h3>
+                <a className="splash-page-footer-link" href="#">Portfolio</a>
+                <a className="splash-page-footer-link" href="https://www.linkedin.com/in/joseph-delaney-46985b76/">LinkedIn</a>
+                <a className="splash-page-footer-link" href="https://github.com/joe-delaney/slack-chat">Github</a>
+            </ul>
+        </div>
     </div>
 )
 
