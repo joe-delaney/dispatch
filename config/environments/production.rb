a6c28fa78e105264300d674c4acbc5bdb1b8f629
joe-delaney <joseph.delaney546@gmail.com>
2022-05-06 11:25:31 -0400

    update config for action cable on heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.web_socket_server_url = "wss://dispatch-client.herokuapp.com/cable" 
+  config.action_cable.allowed_request_origins = ['https://dispatch-client.herokuapp.com', 'http://dispatch-client.herokuapp.com']
   # Code is not reloaded between requests.
   config.cache_classes = true
 
